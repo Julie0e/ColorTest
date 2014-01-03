faaa9eeb40df70d0d365e89b4ba57447eecfcfc2
Julie0e <julie_91@hanmail.net>
2014-01-03 16:26:25 +0900

    ColorTest 완료했습니다.

--- ColorTest/ViewController.m
+++ ColorTest/ViewController.m
@@ -9,15 +9,65 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UISlider *redSlider;
 
+@property (weak, nonatomic) IBOutlet UISlider *greenSlider;
+@property (weak, nonatomic) IBOutlet UISlider *blueSlider;
+@property (weak, nonatomic) IBOutlet UIImageView *colorView;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicator;
 @end
 
 @implementation ViewController
 
+-(IBAction)onColorSliderChanged:(id)sender
+{
+    if ([self.activityIndicator isAnimating]) {
+        [self.activityIndicator stopAnimating];
+    }
+    self.colorView.backgroundColor = [UIColor colorWithRed:self.redSlider.value green:self.greenSlider.value blue:self.blueSlider.value alpha:1.0];
+}
+-(IBAction)powerOnOff:(id)sender
+{
+    BOOL isOn = ((UISwitch *) sender).on;
+    
+    self.redSlider.enabled = isOn;
+    self.greenSlider.enabled = isOn;
+    self.blueSlider.enabled = isOn;
+    
+    if (isOn) {
+        [self.activityIndicator startAnimating];
+        [self performSelector:@selector(onColorSliderChanged:) withObject:nil afterDelay:1.0];
+    }
+    else
+    {
+        self.colorView.backgroundColor = [UIColor grayColor];
+    }
+    
+}
+
+-(void)viewWillAppear:(BOOL)animated
+{
+    [self onColorSliderChanged:nil];
+}
+
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+//	UISwitch *onOff = [[UISwitch alloc] init];
+//    onOff.on = YES;
+//    [onOff addTarget:self action:@selector(powerOnOff:) forControlEvents:UIControlEventValueChanged];
+//    [self.view addSubview:onOff];
+//    
+//    self.colorView = [[UIView alloc] initWithFrame:CGRectMake(20, 60, 280, 150)];
+//    [self.view addSubview:self.colorView];
+//    
+//    self.activityIndicator = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhiteLarge];
+//    self.activityIndicator.center = self.colorView.center;
+//    self.activityIndicator.hidesWhenStopped = YES;
+//    [self.view addSubview:self.activityIndicator];
+    
+    
 }
 
 - (void)didReceiveMemoryWarning
